2ce2ab78f31913c87c9efe2fb7c099e0a10c7f0c
stephen ryan <stephen.ryan11@gmail.com>
2018-05-21 12:44:33 +0100

    new project/delete old files

---
+++ project/Program.cs
@@ -0,0 +1,12 @@
+class Program
+{
+    static void Main()
+    {
+        // Prompt user for minutes exercised 
+        System.Console.Write("Enter how many minutes you exercised: ");
+
+        // Add minutes exercised to total 
+        // Display total minutes exercised to the screen 
+        // Repeat until user quits    
+    }
+}
