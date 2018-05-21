1f243a262c17fbef9a5449f8731db05b050094d8
stephen ryan <stephen.ryan11@gmail.com>
2018-05-21 13:02:45 +0100

    first

--- project (1)/Program.cs
+++ project (1)/Program.cs
@@ -4,6 +4,11 @@ class Program
     {
         // Prompt user for minutes exercised 
         System.Console.Write("Enter how many minutes you exercised: ");
+      
+     string entry = System.Console.ReadLine();
+      
+     
+      
 
         // Add minutes exercised to total 
         // Display total minutes exercised to the screen 
