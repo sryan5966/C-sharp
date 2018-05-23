c54ea92602b0a631d8af63afc39b4865a4f4bf2b
stephen ryan <stephen.ryan11@gmail.com>
2018-05-23 22:03:09 +0100

    ask user question. takes response

--- project/Program.cs
+++ project/Program.cs
@@ -21,27 +21,40 @@ namespace Treehouse.FitnessFrog
                 }
                 else
                 {
-                    // Add minutes exercised to total 
-                    int minutes = int.Parse(entry);
-                  
-                  if(minutes <= 10)
-                  {
-                    Console.WriteLine("better than nothing, am i right");
-                  }
-                  
-                  else if(minutes <= 30){
-                  Console.WriteLine("way to go hot stuff");
-                  }
-                  
-                   else if(minutes <= 60){
-                  Console.WriteLine("you must be a ninja!!");
-                  }
-                  else
-                  {
-                  Console.WriteLine("ok now your just showing off!");
-                  }
-                  
-                    runningTotal = runningTotal + minutes;
+                    try
+                    {
+                        // Add minutes exercised to total 
+                        int minutes = int.Parse(entry);
+
+                        if(minutes <= 0)
+                        {
+                            Console.WriteLine(minutes + " is not an acceptable value.");
+                            continue;
+                        }
+                        else if(minutes <= 10)
+                        {
+                            Console.WriteLine("Better than nothing, am I right?");
+                        }
+                        else if(minutes <= 30)
+                        {
+                            Console.WriteLine("Way to go hot stuff!");
+                        }
+                        else if(minutes <= 60)
+                        {
+                            Console.WriteLine("You must be a ninja warrior in training!");
+                        }
+                        else
+                        {        
+                            Console.WriteLine("Okay, now you're just showing off!");
+                        }
+
+                        runningTotal = runningTotal + minutes;
+                    }
+                    catch(FormatException)
+                    {
+                        Console.WriteLine("That is not valid input.");
+                        continue;
+                    }
 
                     // Display total minutes exercised to the screen 
                     Console.WriteLine("You've entered " + runningTotal + " minutes.");
